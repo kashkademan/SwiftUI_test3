0862f447e7c02d44e01d3d4d16ea761271854e57
Kashkademan <v888.rus@gmail.com>
2019-11-22 16:58:03 -0600

    Lesson 1 completed

---
+++ SwiftUI_test3/CircleImage.swift
@@ -0,0 +1,26 @@
+//
+//  CircleImage.swift
+//  SwiftUI_test3
+//
+//  Created by administrator on 11/22/19.
+//  Copyright © 2019 administrator. All rights reserved.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Image("turtlerock")
+            .clipShape(Circle())
+            .overlay(
+                Circle().stroke(Color.white, lineWidth: 4)
+            )
+            .shadow(radius: 10)
+    }
+}
+
+struct CircleImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImage()
+    }
+}
