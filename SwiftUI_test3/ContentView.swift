67cd3e56d8fa7a98de4b996457f3211fe1b1ca46
Kashkademan <v888.rus@gmail.com>
2019-11-22 15:37:36 -0600

    Initial Commit

---
+++ SwiftUI_test3/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI_test3
+//
+//  Created by administrator on 11/22/19.
+//  Copyright © 2019 administrator. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
